ef8933013fcd406f207e96e0a7188d9174caef08
yangjeremy <390076563@qq.com>
2018-03-19 02:20:43 +0800

    change path

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-resumer-1/dist/',
+    assetsPublicPath: '/Vue_R2/dist/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
